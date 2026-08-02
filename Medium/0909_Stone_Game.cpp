fcc13f01d802c494f4dc53dc281e9cf6f01eb49b
Vivanshu Chauhan <vivanshurajput92@gmail.com>
2026-08-02 11:35:55 +0530

    Sync LeetCode solution: Stone Game (C++)

--- Medium/0909_Stone_Game.cpp
+++ Medium/0909_Stone_Game.cpp
@@ -3,25 +3,27 @@
  * Problem ID: 909
  * Difficulty: Medium
  * Language: C++
- * Runtime: 23 ms
- * Memory: 19.8 MB
+ * Runtime: 0 ms
+ * Memory: 10.7 MB
  * Synced From: LeetCode
  * Date: 2026-08-02
  */
 
 class Solution {
-private:
-    int f(int i,int j,vector<int> &nums,auto &dp){
-        if(i == j) return nums[i];
-        if(dp[i][j] != -1) return dp[i][j];
-        int pickRight = nums[j] - f(i,j-1,nums,dp);
-        int pickLeft = nums[i] - f(i+1,j,nums,dp);
-        return dp[i][j] = max(pickLeft,pickRight);
-    }
 public:
     bool stoneGame(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return f(0,n-1,nums,dp);
+        vector<int> dp(n,-1),curr(n,-1);
+        for(int i=n-1;i>=0;i--){
+            curr[i] = nums[i];
+            dp[i] = nums[i];
+            for(int j=i+1;j<n;j++){
+                int pickRight = nums[j] - curr[j-1];
+                int pickLeft = nums[i] - dp[j];
+                curr[j] = max(pickLeft,pickRight);
+            }
+            swap(dp,curr);
+        }
+        return dp[n-1];
     }
 };
