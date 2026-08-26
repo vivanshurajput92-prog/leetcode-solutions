fabc8c1f8d28fd3f616689c5593152f8c6bc25bc
Vivanshu Chauhan <vivanshurajput92@gmail.com>
2026-08-27 01:39:39 +0530

    Sync LeetCode solution: Sort Colors (C++)

--- Medium/0075_Sort_Colors.cpp
+++ Medium/0075_Sort_Colors.cpp
@@ -4,7 +4,7 @@
  * Difficulty: Medium
  * Language: C++
  * Runtime: 0 ms
- * Memory: 12 MB
+ * Memory: 11.5 MB
  * Synced From: LeetCode
  * Date: 2026-08-26
  */
@@ -13,31 +13,11 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        int ct_0 = 0;
-        int ct_1 = 0;
-        int ct_2 = 0;
         for(int i=0;i<n;i++){
-            if(nums[i] == 0) ct_0++;
-            else if(nums[i] == 1) ct_1++;
-            else if(nums[i] == 2) ct_2++;
+            for(int j=0;j<n;j++){
+                if(j < n-1 && nums[j] == 2) swap(nums[j],nums[j+1]);
+                else if(j > 0 && nums[j] == 0) swap(nums[j-1],nums[j]);
+            }
         }
-        vector<int> ans(n);
-        int i = 0;
-        while(ct_0){
-            ans[i] = 0;
-            ct_0--;
-            i++;
-        }
-        while(ct_1){
-            ans[i] = 1;
-            ct_1--;
-            i++;
-        }
-        while(ct_2){
-            ans[i] = 2;
-            ct_2--;
-            i++;
-        }
-        nums = ans;
     }
 };
