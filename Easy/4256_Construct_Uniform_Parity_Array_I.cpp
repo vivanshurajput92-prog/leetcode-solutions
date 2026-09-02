ff3ec840f078679390590a3190ad2088c0123931
Vivanshu Chauhan <vivanshurajput92@gmail.com>
2026-09-02 19:28:48 +0530

    Sync LeetCode solution: Construct Uniform Parity Array I (C++)

---
+++ Easy/4256_Construct_Uniform_Parity_Array_I.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem: Construct Uniform Parity Array I
+ * Problem ID: 4256
+ * Difficulty: Easy
+ * Language: C++
+ * Runtime: 0 ms
+ * Memory: 30.3 MB
+ * Synced From: LeetCode
+ * Date: 2026-09-02
+ */
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
