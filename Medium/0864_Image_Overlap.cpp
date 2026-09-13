b236204b8e621025ca28c5d985d3c9c9e5e9d595
Vivanshu Chauhan <vivanshurajput92@gmail.com>
2026-09-14 03:22:19 +0530

    Sync LeetCode solution: Image Overlap (C++)

---
+++ Medium/0864_Image_Overlap.cpp
@@ -0,0 +1,34 @@
+/*
+ * Problem: Image Overlap
+ * Problem ID: 864
+ * Difficulty: Medium
+ * Language: C++
+ * Runtime: 7 ms
+ * Memory: 13.8 MB
+ * Synced From: LeetCode
+ * Date: 2026-09-13
+ */
+
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int n = img1.size();
+        vector<pair<int,int>> A, B;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (img1[i][j] == 1) A.emplace_back(i, j);
+                if (img2[i][j] == 1) B.emplace_back(i, j);
+            }
+        }
+        vector<vector<int>> cnt(2 * n, vector<int>(2 * n, 0));
+        int best = 0;
+        for (auto& a : A) {
+            for (auto& b : B) {
+                int dx = b.first - a.first + n;
+                int dy = b.second - a.second + n;
+                best = max(best, ++cnt[dx][dy]);
+            }
+        }
+        return best;
+    }
+};
